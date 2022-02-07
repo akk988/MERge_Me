cd525979ceb71d0d28d27f09708a09ddbac3e396
AKK <akk070ipa@gmail.com>
2022-02-08 00:53:31 +0100

    Implement Merge and Quicksort methods

--- helpers.py
+++ helpers.py
@@ -26,7 +26,22 @@ class Merge(object):
 
         # defining an array called stack to store the merged intervals
         stack = []
-
+        # Adding the first (smallest) interval to the stack
+        stack.append(intervals[0])
+        # Comparing the end of the interval appended to the stack (called "last_element") to the end of the next intervalls in a for loop
+        for i in range(1, len(intervals)):
+            last_element = stack[len(stack)-1]
+            # comparing the end (e.g. 3) of the first interval (e.g. [1, 3]) with the start (e.g. 1) of the second/next range (e.g. [1, 5])
+            # In case there is an overlaping, then a merge will be operated
+            if last_element[1] >= intervals[i][0]:
+                # set the end of the last_element to be equal to the larger number between the two ends of the compared intervalls (e.g. 5 > 3 --> end of last-element = 5)
+                last_element[1] = max(intervals[i][1], last_element[1])
+                # deleting the first interval ( [1, 3]) from the stack and adding the extended last_element
+                stack.pop(len(stack)-1)
+                stack.append(last_element)
+            # In case there is no overlaping, the interval will be added to the stack as it is.
+            else:
+                stack.append(intervals[i])
         return stack
 
     # define a method which takes an array and a range (e.g. from 0 to 5) and return a pivot-index to be used in the quicksort method
