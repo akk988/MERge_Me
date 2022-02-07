41681c18350a8a35db92a81e3923554414e9931a
AKK <akk070ipa@gmail.com>
2022-02-08 00:50:16 +0100

    Implement Quicksort method and Add constants for testing

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 # Variables to be used for developing and testing the project
 
-INTERVALS = [[1, 3], [1, 5], [3, 7], [11, 14], [8, 10], [15, 18]]
+#INTERVALS = [[1, 3], [1, 5], [3, 7], [11, 14], [8, 10], [15, 18]]
+INTERVALS = [[1, 2], [1, 7], [8, 11], [11, 14],
+             [8, 10], [32, 36], [33, 35], [105, 108]]
--- helpers.py
+++ helpers.py
@@ -9,7 +9,10 @@ class Merge(object):
 
     # define method called merge with two or more intervals as arguement and returns merged intervals as a stack
     def merge(self, intervals):
-
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
         # return an empty array if no intervals where entered
         if len(intervals) == 0:
             return []
@@ -30,16 +33,26 @@ class Merge(object):
     def partition(self, array, start, end):
         # initializing pivot's index equal to the start of the range (e. g. 0)
         pivot_index = start
-
+        for i in range(start, end):
+            # place all elements smaller than the pivot to the left of the pivot
+            if array[i][0] <= array[end][0]:
+                array[i], array[pivot_index] = array[pivot_index], array[i]
+                pivot_index += 1
+        array[end], array[pivot_index] = array[pivot_index], array[end]
         return pivot_index
 
     # define a method which partitions the given array around a picked pivot (partition_index).
     def quicksort(self, array, start, end):
+        if start < end:
+            partition_index = self.partition(array, start, end)
+            self.quicksort(array, start, partition_index-1)
+            self.quicksort(array, partition_index + 1, end)
 
-        # Creating new instance of the class
-        #mer = Merge()
-        #intervalls = input()
-        #print('Input, ' + intervalls)
-        # Calling merge method of class Merge and passing in the intervals
-        #print('Output, ' + mer.merge([intervalls]))
-        #print(mer.merge([[1, 3], [1, 5], [3, 7], [11, 14], [8, 10], [15, 18]]))
+
+# Creating new instance of the class
+#mer = Merge()
+#intervalls = input()
+#print('Input, ' + intervalls)
+# Calling merge method of class Merge and passing in the intervals
+#print('Output, ' + mer.merge([intervalls]))
+#print(mer.merge([[1, 3], [1, 5], [3, 7], [11, 14], [8, 10], [15, 18]]))
